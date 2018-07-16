b0da8d951f2113a3c4e66eae15b5c36a8faa1946
Paul Yushkevich <pyushkevich@gmail.com>
2018-07-16 13:34:34 -0400

    about to add user rotation into viz space

--- counterstain/copy.sh
+++ counterstain/copy.sh
@@ -1,39 +1,81 @@
 #!/bin/bash
+set -x -e
 
 # Point to the latest Greedy and c3d 
 export PATH=/Users/pauly/tk/lddmm/xc64rel:/Users/pauly/tk/convert3d/xc64rel:$PATH
 
-MANIFEST=HNL_11_15_HR3p_tau.txt
+MANIFEST=${1?}
+PAT=$(cat $MANIFEST | awk '{printf "%s_%s\n",$2,$3}' | sort -u)
 
-mkdir -p slides
+function slice_vars()
+{
+  SLIDEID=$(printf %04d $SLIDE)
 
-while read -r SVS ID BLOCK SLIDE STAIN FLIP; do
-
-  out=slides/${ID}_${BLOCK}_tau_${SLIDE}_mrilike.nii.gz
+  out=slides/${ID}_${BLOCK}_tau_${SLIDEID}_mrilike.nii.gz
   remote=chead:/data/picsl/pauly/tau_atlas/exp01/HNL_11_15/slides/${SVS}/${SVS}_mrilike.nii.gz
-  fixed=slides/${ID}_${BLOCK}_tau_${SLIDE}_mrilike_fix.nii.gz
-  tearfix=slides/${ID}_${BLOCK}_tau_${SLIDE}_mrilike_tearfix.nii.gz
+  fixed=slides/${ID}_${BLOCK}_tau_${SLIDEID}_mrilike_fix.nii.gz
+  tearfix=slides/${ID}_${BLOCK}_tau_${SLIDEID}_mrilike_tearfix.nii.gz
+
+  # The RGB thumbnail of histology - for pretty images
+  thumb=../aperio/matching/tau/${SVS}.svs_thumbnail.tiff
 
   # Extract the MRI slice
-  mri_slice=slides/${ID}_${BLOCK}_mri_${SLIDE}.nii.gz
+  mri_slice=slides/${ID}_${BLOCK}_mri_${SLIDEID}.nii.gz
 
   # The MRI volume for this block
   mri_vol=../work/${ID}_${BLOCK}_mri_toblock_affine.nii.gz
 
+  # High-resolution MRI slide and volume
+  hires_slice=slides/${ID}_${BLOCK}_hires_${SLIDEID}.nii.gz
+  hires_vol=../work/${ID}_${BLOCK}_hires_mri_toblock_warped.nii.gz
+
+  hires_mask_slice=slides/${ID}_${BLOCK}_hires_mask_${SLIDEID}.nii.gz
+  hires_mask_vol=../work/${ID}_${BLOCK}_hires_mri_mask_toblock_warped.nii.gz
+
   # Rigid transform between MRI (fixed) and histo (moving)
-  mri_hist_rigid=slides/${ID}_${BLOCK}_mri_to_hist_rigid_${SLIDE}.mat
-  mri_hist_rigid_reslice=slides/${ID}_${BLOCK}_mri_to_hist_rigid_reslice_${SLIDE}.nii.gz
+  mri_hist_rigid=slides/${ID}_${BLOCK}_mri_to_hist_rigid_${SLIDEID}.mat
+  mri_hist_rigid_reslice=slides/${ID}_${BLOCK}_mri_to_hist_rigid_reslice_${SLIDEID}.nii.gz
+
+  # Tear fixed with adjusted spacing to allow rigid registration
+  tearfix_rescaled=slides/${ID}_${BLOCK}_tau_${SLIDEID}_mrilike_tearfix_rescaled.nii.gz
+  thumb_rescaled=slides/${ID}_${BLOCK}_tau_${SLIDEID}_thumb_rescaled.nii.gz
+  mri_hist_rigid_rescaled=slides/${ID}_${BLOCK}_mri_to_hist_rigid_rescaled_${SLIDEID}.mat
+  mri_hist_rigid_rescaled_reslice=slides/${ID}_${BLOCK}_mri_to_hist_rigid_reslice_rescaled_${SLIDEID}.nii.gz
+
+  mri_hist_rescaled_warp=slides/${ID}_${BLOCK}_mri_to_hist_warp_rescaled_${SLIDEID}.nii.gz
+  mri_hist_rescaled_warp_reslice=slides/${ID}_${BLOCK}_mri_to_hist_warp_reslice_rescaled_${SLIDEID}.nii.gz
+  mri_hist_rgb_rescaled_warp_reslice=slides/${ID}_${BLOCK}_mri_to_hist_rgb_warp_reslice_rescaled_${SLIDEID}.nii.gz
+
+  # Tau stuff
+  tau_density=$(ls milad/${SVS}_job_*_analysis_Area_SW20x20.nii.gz)
+  tau_density_smooth=slides/${ID}_${BLOCK}_tau_density_smooth.nii.gz 
+  tau_density_rescaled=slides/${ID}_${BLOCK}_tau_density_${SLIDEID}_thumb_rescaled.nii.gz
+  mri_tau_density_rescaled_warp_reslice=slides/${ID}_${BLOCK}_mri_to_tau_density_warp_reslice_rescaled_${SLIDEID}.nii.gz
+
+  # Do we need to flip
+  FLIPCMD=$(echo $FLIP | sed -e "s/1/-flip x/" -e "s/0/-flip x/")
+
+  # Final slice images mapped into space for visualization
+  mri_viz=slides/${ID}_${BLOCK}_viz_mri_${SLIDEID}.nii.gz
+  hires_viz=slides/${ID}_${BLOCK}_viz_hires_${SLIDEID}.nii.gz
+  mri_hist_rescaled_warp_viz_reslice=slides/${ID}_${BLOCK}_viz_mri_to_hist_warp_reslice_rescaled_${SLIDEID}.nii.gz
+  mri_hist_rgb_rescaled_warp_viz_reslice=slides/${ID}_${BLOCK}_viz_mri_to_hist_rgb_warp_reslice_rescaled_${SLIDEID}.nii.gz
+  mri_tau_density_rescaled_warp_viz_reslice=slides/${ID}_${BLOCK}_viz_mri_to_tau_density_warp_reslice_rescaled_${SLIDEID}.nii.gz
+}
+
+<<'SKIP1'
+mkdir -p slides
+
+while read -r SVS ID BLOCK SLIDE STAIN FLIP; do
+
+  # Read the vars
+  slice_vars
 
   # Download the slide if it does not exist
-  echo $SVS $SLIDE
+  echo $SVS $SLIDEID
   if [[ ! -f $out ]]; then
     scp $remote $out
   fi
-
-<<'SKIPME'  
-
-  # Do we need to flip
-  FLIPCMD=$(echo $FLIP | sed -e "s/1/-flip x/" -e "s/0//")
 
   # Make a negative and fix the spacing and also flip if necessary
   c2d $out -clip 0 1 -stretch 0 1 1 0 -spacing 0.05x0.05mm \
@@ -45,13 +87,18 @@ while read -r SVS ID BLOCK SLIDE STAIN FLIP; do
     -push G -push M -replace 0 1 1 0 -times \
     -add -o $tearfix
 
-
   # Extract the MRI slice
-  c3d $mri_vol -slice z $((SLIDE-1)) -o $mri_slice
+  c3d $mri_vol -slice z $((SLIDE)) -o $mri_slice
   c2d $mri_slice -o $mri_slice
 
-SKIPME
 
+  # Extract the high-resolution MRI slice
+  c3d $hires_vol -slice z $((SLIDE)) -o $hires_slice
+  c2d $hires_slice -o $hires_slice
+  c3d $hires_mask_vol -slice z $((SLIDE)) -o $hires_mask_slice
+  c2d $hires_mask_slice -o $hires_mask_slice
+
+<<'SKIP2'
   # Affine registration between MRI and remapped histology
   greedy -d 2 \
     -a -i $mri_slice $tearfix -m NCC 11x11 -o $mri_hist_rigid \
@@ -63,4 +110,125 @@ SKIPME
     -r $mri_hist_rigid
 
 done < $MANIFEST
+
+SKIP1
+
+# Figure out the common scaling factor
+for fn in $(ls slides/${PAT}_mri_to_hist_rigid_*.mat); do 
+  cat $fn | awk 'NR<=2 { print $1,$2,0,$3 } NR==3 { print 0,0,1,0 } END { print 0,0,0,1 }' > /tmp/temp.mat
+  c3d_affine_tool /tmp/temp.mat -info-full | grep Affine
+done \
+  | sed -e "s/.*S = .//" -e "s/.; K =.*//" -e "s/,/ /g" \
+  | awk '{printf "%f\n%f\n",$1,$2}' | sort -n > /tmp/scales.txt
+MEDIAN_SCALE=$(cat /tmp/scales.txt | head -n $(echo $(( $(cat /tmp/scales.txt | wc -l) / 2))) \
+  | tail -n 1 | awk '{print $1 / 100.}')
+
+# Work out the viz matrix
+viz_matrix=../viewmatrix.mat
+viz_mat2d=slides/viewmatrix_2d.mat
+cat $viz_matrix \
+  | awk 'NR==1 {print $1,$2,$4} NR==2 {print $1,$2,$4} END {print 0,0,1}' \
+  > $viz_mat2d
+
+# Try to figure out the common scaling factor and apply it to the voxel
+while read -r SVS ID BLOCK SLIDE STAIN FLIP; do
+
+  # Slice variables
+  slice_vars
+
+<<'SKIP3'
+
+  # Adjust the voxel size 
+  new_spacing=$(c2d $tearfix -info-full | grep Spacing \
+    | sed -e "s/.*:..//" -e "s/.$//" -e "s/,//g" \
+    | awk -v k=1.09 '{printf "%fx%fmm\n", $1/k, $2/k}')
+
+  c2d $tearfix -spacing $new_spacing -o $tearfix_rescaled
+
+  # Rigid registration between MRI and remapped histology
+  greedy -d 2 \
+    -a -i $mri_slice $tearfix_rescaled -m NCC 11x11 -o $mri_hist_rigid_rescaled \
+    -n 40x40x20 -dof 6 -ia-image-centers -search 10000 90 5
+
+  # Reslice the histology to MRI
+  greedy -d 2 \
+    -rf $mri_slice -rm $tearfix_rescaled $mri_hist_rigid_rescaled_reslice \
+    -r $mri_hist_rigid_rescaled
+
+  # Registration to the low-resolution MRI, no mask
+  # greedy -d 2 -sv \
+  #   -i $mri_slice $tearfix_rescaled -m NCC 11x11 -o $mri_hist_rescaled_warp \
+  #   -n 40x40x20 -it $mri_hist_rigid_rescaled \
+  #   -s 10.0vox 1.0vox
+  greedy -d 2 -sv \
+    -i $hires_slice $tearfix_rescaled -m NCC 11x11 -o $mri_hist_rescaled_warp \
+    -n 40x40x20 -it $mri_hist_rigid_rescaled \
+    -gm $hires_mask_slice \
+    -s 10.0vox 1.0vox
+
+  greedy -d 2 \
+    -rf $mri_slice -rm $tearfix_rescaled $mri_hist_rescaled_warp_reslice \
+    -r $mri_hist_rescaled_warp $mri_hist_rigid_rescaled 
+
+
+  # Apply the transformation to the thumb SVS image
+
+  # The the dimensions of the processed counterstain image
+  DIM=$(c2d $tearfix_rescaled -info-full | grep Dimensions \
+    | sed -e "s/.*: .//" -e "s/.$//" -e "s/, /x/g")
+
+  # Adjust the SVS thumbnail to have the same dimensions
+  c2d $tearfix_rescaled -popas T -mcs $thumb \
+    -foreach -flip x -resample $DIM -insert T 1 -copy-transform -endfor \
+    -omc $thumb_rescaled
+
+  greedy -d 2 \
+    -rf $mri_slice -rm $thumb_rescaled $mri_hist_rgb_rescaled_warp_reslice \
+    -r $mri_hist_rescaled_warp $mri_hist_rigid_rescaled 
+
+
+  # Apply the transformation to the tau density maps
+
+  # The the dimensions of the processed counterstain image
+  DIM=$(c2d $tearfix_rescaled -info-full | grep Dimensions \
+    | sed -e "s/.*: .//" -e "s/.$//" -e "s/, /x/g")
+
+  # Adjust the SVS thumbnail to have the same dimensions
+  c2d $tau_density -int 0 -resample 5x5% -smooth-fast 3vox -o $tau_density_smooth
+  c3d $tearfix_rescaled \
+    $tau_density_smooth -swapdim PLI \
+    -flip x -resample ${DIM}x1 -copy-transform \
+    -o $tau_density_rescaled
+
+  c2d $tau_density_rescaled -o $tau_density_rescaled
+
+  greedy -d 2 \
+    -rf $mri_slice -rm $tau_density_rescaled $mri_tau_density_rescaled_warp_reslice \
+    -r $mri_hist_rescaled_warp $mri_hist_rigid_rescaled 
+
+SKIP3
+
+  # Reconstruct slices into viz space (this is getting uggly)
+  greedy -d 2 \
+    -rf $mri_slice -rm $mri_slice $mri_viz -r $viz_mat2d
+
+  exit
+
+
+done < $MANIFEST
+
+
+# Create 3D images
+mkdir -p recon
+for what in mri hires mri_to_hist_warp_reslice_rescaled mri_to_tau_density_warp_reslice_rescaled; do
+  c3d slides/${PAT}_${what}_????.nii.gz -tile z -o recon/${PAT}_${what}.nii.gz
+done
+
+for what in mri_to_hist_rgb_warp_reslice_rescaled; do
+
+  c3d -mcs slides/${PAT}_${what}_????.nii.gz \
+    -foreach-comp 3 -tile z -endfor -omc recon/${PAT}_${what}.nii.gz
+
+done
+
 
--- runme.sh
+++ runme.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-PATH=/home/pauly/tk/convert3d/xc64rel:/Users/pauly/tk/ants/xc64rel/bin:$PATH
+set -x -e
+PATH=/Users/pauly/tk/convert3d/xc64rel:/Users/pauly/tk/ants/xc64rel/bin:$PATH
+PATH=/Users/pauly/tk/lddmm/xc64rel:$PATH
 PATH=/Library/Frameworks/R.framework/Versions/3.3/Resources/bin/:$PATH
+echo $PATH
 
 # The ID
 ID=${1?}
@@ -25,6 +28,13 @@ MRI_MASK_MOLD=$WORK/tissue_mask.nii.gz
 HIRES_MRI=${ID}_mri_hires.nii.gz
 HIRES_MRI_AFFINE=mri_hires_to_lores_affine.mat
 
+# The mask for the high-resolution MRI
+HIRES_REGMASK=$WORK/${ID}_mri_hires_mask.nii.gz
+HIRES_WARP=$WORK/${ID}_mri_warp_fx_hires_mv_lores.nii.gz
+HIRES_ROOT_WARP=$WORK/${ID}_mri_rootwarp_fx_hires_mv_lores.nii.gz
+HIRES_INV_WARP=$WORK/${ID}_mri_invwarp_fx_hires_mv_lores.nii.gz
+RESLICE_MRI_TO_HIRES=$WORK/${ID}_mri_lores_reslice_to_hires.nii.gz
+
 # Get the origin of the slit mold
 MOLD_CENTER=$(c3d $MOLD -probe 50% | awk '{print $5,$6,$7}')
 
@@ -40,7 +50,25 @@ c3d $MOLD $CONTOUR -shift -1 -reslice-matrix $HOLDERMAT -thresh -inf -1 1 0 -o $
 c3d $CONTOUR -thresh -inf 0 1 0 -type uchar -o $MRI_MASK_NATIVE
 N4BiasFieldCorrection -d 3 -i $MRI -o $MRI_N4 -x $MRI_MASK_NATIVE
 
+
+
+# Additional processing of the hi-resolution image
+
+# Generate the high-resolution image mask
+c3d $HIRES_MRI -cmv -thresh 5 inf 1 0 -o $HIRES_REGMASK
+
+# Registration with high-resolution image as fixed, low-resolution as moving, lots of smoothness
+greedy -d 3 -i $HIRES_MRI $MRI_N4 -it $HIRES_MRI_AFFINE,-1 \
+  -o $HIRES_WARP -oroot $HIRES_ROOT_WARP -sv -s 3mm 0.2mm -m NCC 8x8x8 -n 40x40x0 -gm $HIRES_REGMASK \
+  -wp 0.0001 -exp 6 \
+
+
+# Apply the registration
+greedy -d 3 -rf $HIRES_MRI -rm $MRI_N4 $RESLICE_MRI_TO_HIRES -r $HIRES_ROOT_WARP,64 $HIRES_MRI_AFFINE,-1 
+greedy -d 3 -rf $HIRES_MRI -rc $HIRES_INV_WARP -wp 0.001 -r $HIRES_ROOT_WARP,-64
+
 SKIP1
+
 
 # Position each of the blockface slabs relative to the slit mold. This requires flipping
 # in the z direction, and setting the origin of the center voxel
@@ -68,6 +96,8 @@ while read -r BLOCK_ID BLOCK_Z FLIPS ROT_INIT DX_INIT DY_INIT; do
 
   # Hires MRI resampled to block
   HIRES_MRI_TO_BLOCK_AFFINE=$WORK/${ID}_${BLOCK_ID}_hires_mri_toblock_affine.nii.gz
+  HIRES_MRI_TO_BLOCK_WARPED=$WORK/${ID}_${BLOCK_ID}_hires_mri_toblock_warped.nii.gz
+  HIRES_MRI_MASK_TO_BLOCK_WARPED=$WORK/${ID}_${BLOCK_ID}_hires_mri_mask_toblock_warped.nii.gz
 
   # Intensity remapped histology block
   BLOCK_FN_TOMOLD_INIT_REMAPPED=$WORK/${ID}_${BLOCK_ID}_blockface_tomold_init_remapped.nii.gz
@@ -76,14 +106,14 @@ while read -r BLOCK_ID BLOCK_Z FLIPS ROT_INIT DX_INIT DY_INIT; do
 
   # Flip and set the origin of the blockface image and also extract the negative of the 
   # green channel, which seems to have the best contrast
-  /Users/pauly/tk/convert3d/xc64rel/c3d -mcs $BLOCK_FN \
+  /Users/pauly/tk/convert3d/xc64rel/c3d -verbose -mcs $BLOCK_FN \
     -foreach $FLIPCMD -origin-voxel-coord 50% $BLOCK_ORIGIN -endfor \
     -omc $BLOCK_FN_TOMOLD_INIT \
     -pop -stretch 0 255 255 0 -o $BLOCK_FN_TOMOLD_INIT_INVGREEN
 
   # Reslice the MRI into the space of the histology block and make it RGB
-  c3d $BLOCK_FN_TOMOLD_INIT $MRI_N4 -reslice-matrix $HOLDERMAT -o $MRI_TO_BLOCK_INIT
-  c3d $BLOCK_FN_TOMOLD_INIT $MRI_MASK_MOLD -reslice-identity -o $MRI_TO_BLOCK_INIT_MASK
+  c3d -verbose $BLOCK_FN_TOMOLD_INIT $MRI_N4 -reslice-matrix $HOLDERMAT -o $MRI_TO_BLOCK_INIT
+  c3d -verbose $BLOCK_FN_TOMOLD_INIT $MRI_MASK_MOLD -reslice-identity -o $MRI_TO_BLOCK_INIT_MASK
 
   # Generate initial rotation matrix
   ROT_CTR=$(echo $BLOCK_ORIGIN | sed -e "s/x/ /g")
@@ -93,22 +123,27 @@ while read -r BLOCK_ID BLOCK_Z FLIPS ROT_INIT DX_INIT DY_INIT; do
   # Perform the rigid, then affine registration
   greedy -d 3 -a -dof 6 -i $MRI_TO_BLOCK_INIT $BLOCK_FN_TOMOLD_INIT_INVGREEN \
     -gm $MRI_TO_BLOCK_INIT_MASK -o $BLOCK_TO_MRI_RIGID_INVGREEN \
-    -m NCC 4x4x1 -n 60x40x10 -ia $BLOCK_TO_MRI_RIGID_INIT
+    -m NCC 4x4x4 -n 60x40x0 -ia $BLOCK_TO_MRI_RIGID_INIT
 
   greedy -d 3 -a -dof 12 -i $MRI_TO_BLOCK_INIT $BLOCK_FN_TOMOLD_INIT_INVGREEN \
     -gm $MRI_TO_BLOCK_INIT_MASK -o $BLOCK_TO_MRI_AFFINE_INVGREEN \
-    -m NCC 4x4x1 -n 60x40x10 -ia $BLOCK_TO_MRI_RIGID_INVGREEN
+    -m NCC 4x4x4 -n 60x40x0 -ia $BLOCK_TO_MRI_RIGID_INVGREEN
 
   # Reslice the MRI into block space using the affine registration result
   greedy -d 3 -rf $MRI_TO_BLOCK_INIT_MASK -rm $MRI_N4 $MRI_TO_BLOCK_AFFINE \
     -ri LABEL 0.2vox -rm $MRI_MASK_NATIVE $MRI_TO_BLOCK_AFFINE_MASK \
     -r $BLOCK_TO_MRI_AFFINE_INVGREEN,-1 $HOLDERMAT
   
-SKIP2
-
-  # Reslice the high-resolution MRI as well
+  # Reslice the high-resolution MRI as well. Also reslice the high-resolution MRI mask, 
+  # so we can perform registration to histology later
   greedy -d 3 -rf $MRI_TO_BLOCK_INIT_MASK -rm $HIRES_MRI $HIRES_MRI_TO_BLOCK_AFFINE \
     -r $BLOCK_TO_MRI_AFFINE_INVGREEN,-1 $HOLDERMAT $HIRES_MRI_AFFINE
+
+SKIP2
+  greedy -d 3 -rf $MRI_TO_BLOCK_INIT_MASK -rm $HIRES_MRI $HIRES_MRI_TO_BLOCK_WARPED \
+    -ri NN -rm $HIRES_REGMASK $HIRES_MRI_MASK_TO_BLOCK_WARPED \
+    -r $BLOCK_TO_MRI_AFFINE_INVGREEN,-1 $HOLDERMAT $HIRES_MRI_AFFINE $HIRES_INV_WARP 
+
 
 
 done < manifest.txt
